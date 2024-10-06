463eed768c90eaf00c3bb8e0c4c64fbb0803e974
Hwiseong <hw969655@dal.ca>
2024-10-06 14:15:21 -0300

    finished co2 heatmap and algaeheatmao

--- src/alberta_map.py
+++ src/alberta_map.py
@@ -6,7 +6,7 @@ import json
 
 def generateAlbertaMap(option, year):
     global albertamap
-    albertamap = folium.Map(location=[56.1304, -90.3468], tiles="OpenStreetMap", zoom_start=7)
+    albertamap = folium.Map(location=[55, -115.5834], tiles="OpenStreetMap", zoom_start=5)
     df = pd.read_csv("res/carbonemissions.csv")
     generate_alberta_heatmap(option, year) 
     albertamap.save('src/static/albertaMap.html')
@@ -36,8 +36,25 @@ def generate_alberta_heatmap(option, year):
 
     
     #make if statement for CO2
+    elif option == "co2":
+        co2_values = [float(num["co2_Value"]) for num in data]
+        max_co2 = max(co2_values)
+    #create a loop that iterates through co2 values, finds the associated "Region" and gets the 'lon' and 'lat' value from
+    # the coordinates dataFrame and adds it as a new list into the heat map data list
+        heatmap_data = []
+        for city in data:
+            city_name = city['city_name']
 
-    
+            lat = float(city['latitude'])
+            lon = float(city['longitude'])
+            value = float(city['co2_Value'])/max_co2 #divide by the max value to get our opacity value between 0 and 1
+
+            # Add the data to heatmap_data
+            heatmap_data.append([lat, lon, value])  # Append [latitude, longitude, CO2 value]
+
+
+
+
     HeatMap(heatmap_data, radius=50, blur=40, min_opacity=0.2).add_to(albertamap)
     
     # for row in data:
